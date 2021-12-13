bcd15d4492fc52df367ba8da02717f2f75b09097
StrayKite\jonathan <jonathan@straykitestudios.com>
2021-12-12 18:45:07 -0600

    Day 10

--- Source/Day10.py
+++ Source/Day10.py
@@ -6,7 +6,7 @@ def print_count(countValue):
     print(f'Total Count: {countValue}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def split(word):
-    return [int(char) for char in word]
+    return [char for char in word]
 
 def IsValidIndex( listToCheck:list, index):
     if 0 <= index < len(listToCheck):
@@ -25,13 +25,98 @@ def SolveDayPartA(filepath):
     with open(filepath, "r") as openedFile:
         fileData = openedFile.readlines()
 
-    return 0
+    illegalParen = 0
+    illegalBracket = 0
+    illegalAngle = 0
+    illegalCurly = 0
+    for fileLine in fileData:
+        charList = split(fileLine.strip())
+        lineStack = []
+        for char in charList:
+            if char == '[':
+                lineStack.append(char)
+            elif char == '(':
+                lineStack.append(char)
+            elif char == '<':
+                lineStack.append(char)
+            elif char == '{':
+                lineStack.append(char)
+            elif char == ']':
+                lastChar = lineStack.pop()
+                if lastChar != '[':
+                    illegalBracket += 1
+                    break
+            elif char == ')':
+                lastChar = lineStack.pop()
+                if lastChar != '(':
+                    illegalParen += 1
+                    break
+            elif char == '>':
+                lastChar = lineStack.pop()
+                if lastChar != '<':
+                    illegalAngle += 1
+                    break
+            elif char == '}':
+                lastChar = lineStack.pop()
+                if lastChar != '{':
+                    illegalCurly += 1
+                    break
+    return illegalParen*3 + illegalBracket*57 + illegalCurly*1197 + illegalAngle*25137
 
 def SolveDayPartB(filepath):
     with open(filepath, "r") as openedFile:
         fileData = openedFile.readlines()
 
-    return 0
+    incompleteRowScores = []
+    for fileLine in fileData:
+        charList = split(fileLine.strip())
+        lineStack = []
+        isInvalidLine = False
+        for char in charList:
+            if char == '[':
+                lineStack.append(char)
+            elif char == '(':
+                lineStack.append(char)
+            elif char == '<':
+                lineStack.append(char)
+            elif char == '{':
+                lineStack.append(char)
+            elif char == ']':
+                lastChar = lineStack.pop()
+                if lastChar != '[':
+                    isInvalidLine = True
+                    break
+            elif char == ')':
+                lastChar = lineStack.pop()
+                if lastChar != '(':
+                    isInvalidLine = True
+                    break
+            elif char == '>':
+                lastChar = lineStack.pop()
+                if lastChar != '<':
+                    isInvalidLine = True
+                    break
+            elif char == '}':
+                lastChar = lineStack.pop()
+                if lastChar != '{':
+                    isInvalidLine = True
+                    break
+        if len(lineStack) > 0 and isInvalidLine == False:
+            rowVal = 0
+            while len(lineStack) > 0:
+                rowVal *= 5
+                currentChar = lineStack.pop()
+                if currentChar == '(':
+                    rowVal += 1
+                elif currentChar == '[':
+                    rowVal += 2
+                elif currentChar == '{':
+                    rowVal += 3
+                elif currentChar == '<':
+                    rowVal += 4
+            incompleteRowScores.append(rowVal)
+    incompleteRowScores.sort()
+    return statistics.median(incompleteRowScores)
 
 filePath = "C:\\dev\\AdventOfCode\\Input\\Day10.txt"
 print_count(SolveDayPartA(filePath))
